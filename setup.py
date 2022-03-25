682ce201e16821faf88af3531fb51d3a76006a55
Babis <babis@rais.io>
2022-03-25 11:31:56 +0000

    Added gitignore, bumped up module version, added Release Notes file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 NAME = 'django-query-signals'
 DESCRIPTION = 'Django Query Signals'
-VERSION = '0.0.2'
+VERSION = '0.1.0'
 AUTHOR = 'Martin P. Hellwig'
 AUTHOR_EMAIL = 'martin.hellwig@gmail.com'
 URL_MAIN = "https://repository.example.com/" + NAME + '/'
@@ -15,26 +15,34 @@ URL_DOWNLOAD = URL_MAIN + 'download/' + VERSION + '.zip'
 KEYWORDS = [
     'django',
     'django-integrator'
-    ]
+]
 
 CLASSIFIERS = [
     'Programming Language :: Python :: 3',
-    ]
+]
 
 REQUIREMENTS = [
     'Django',
     'django-integrator',
-    ]
+]
 
 LICENSE = 'BSD'
 
 ################################################################################
 
 KWARGS = {
-    'name':NAME, 'packages':find_packages(), 'version':VERSION,
-    'description':DESCRIPTION, 'author':AUTHOR, 'author_email':AUTHOR_EMAIL,
-    'url':URL_MAIN, 'download_url':URL_DOWNLOAD, 'keywords':KEYWORDS,
-    'license':LICENSE, 'classifiers':CLASSIFIERS,
-    'install_requires':REQUIREMENTS}
+    'name': NAME,
+    'packages': find_packages(),
+    'version': VERSION,
+    'description': DESCRIPTION,
+    'author': AUTHOR,
+    'author_email': AUTHOR_EMAIL,
+    'url': URL_MAIN,
+    'download_url': URL_DOWNLOAD,
+    'keywords': KEYWORDS,
+    'license': LICENSE,
+    'classifiers': CLASSIFIERS,
+    'install_requires': REQUIREMENTS
+}
 
 setup(**KWARGS)
