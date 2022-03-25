94db25c6423ff40020bda400703dcc3c265f9027
Babis <babis@rais.io>
2022-03-25 10:22:40 +0000

    Removed providing_args Signal argument (deprecated in django 4)

--- django_query_signals/signals.py
+++ django_query_signals/signals.py
@@ -5,23 +5,27 @@ This module is imported on app ready (see __init__).
 from django.db.models.query import QuerySet
 from django.dispatch import Signal
 
-# pylint: disable=invalid-name
-pre_delete = Signal(providing_args=["args"])
-post_delete = Signal(providing_args=["args"])
-pre_update = Signal(providing_args=["args"])
-post_update = Signal(providing_args=["args"])
-pre_bulk_create = Signal(providing_args=["args"])
-post_bulk_create = Signal(providing_args=["args"])
-pre_get_or_create = Signal(providing_args=["args"])
-post_get_or_create = Signal(providing_args=["args"])
-pre_update_or_create = Signal(providing_args=["args"])
-post_update_or_create = Signal(providing_args=["args"])
 
-METHODS = {'bulk_create':QuerySet.bulk_create,
-           'get_or_create':QuerySet.get_or_create,
-           'update_or_create':QuerySet.update_or_create,
-           'delete':QuerySet.delete,
-           'update':QuerySet.update}
+# pylint: disable=invalid-name
+pre_delete = Signal()
+post_delete = Signal()
+pre_update = Signal()
+post_update = Signal()
+pre_bulk_create = Signal()
+post_bulk_create = Signal()
+pre_get_or_create = Signal()
+post_get_or_create = Signal()
+pre_update_or_create = Signal()
+post_update_or_create = Signal()
+
+METHODS = {
+    'bulk_create': QuerySet.bulk_create,
+    'get_or_create': QuerySet.get_or_create,
+    'update_or_create': QuerySet.update_or_create,
+    'delete': QuerySet.delete,
+    'update': QuerySet.update
+}
+
 
 def _bulk_create(self, objs, batch_size=None, ignore_conflicts=True):
     _ = {'self':self, 'objs':objs, 'batch_size':batch_size,
@@ -31,23 +35,24 @@ def _bulk_create(self, objs, batch_size=None, ignore_conflicts=True):
     post_bulk_create.send(sender=self.model, args=_)
     return _['return']
 
+
 def _get_or_create(self, defaults=None, **kwargs):
     _ = {'self':self, 'defaults':defaults, 'kwargs':kwargs,
          'method':'get_or_create'}
     pre_get_or_create.send(sender=self.model, args=_)
-    _['return'] = METHODS['get_or_create'](
-                                        _['self'], _['defaults'], **_['kwargs'])
+    _['return'] = METHODS['get_or_create'](_['self'], _['defaults'], **_['kwargs'])
     post_get_or_create.send(sender=self.model, args=_)
     return _['return']
+
 
 def _update_or_create(self, defaults=None, **kwargs):
     _ = {'self':self, 'defaults':defaults, 'kwargs':kwargs,
          'method':'update_or_create'}
     pre_update_or_create.send(sender=self.model, args=_)
-    _['return'] = METHODS['update_or_create'](
-                                        _['self'], _['defaults'], **_['kwargs'])
+    _['return'] = METHODS['update_or_create'](_['self'], _['defaults'], **_['kwargs'])
     post_update_or_create.send(sender=self.model, args=_)
     return _['return']
+
 
 def _delete(self):
     _ = {'self':self, 'method':'delete'}
@@ -56,12 +61,14 @@ def _delete(self):
     post_delete.send(sender=self.model, args=_)
     return _['return']
 
+
 def _update(self, **kwargs):
     _ = {'self':self, 'kwargs':kwargs, 'method':'update'}
     pre_update.send(sender=self.model, args=_)
     _['return'] = METHODS['update'](_['self'], **_['kwargs'])
     post_update.send(sender=self.model, args=_)
     return _['return']
+
 
 QuerySet.bulk_create = _bulk_create
 QuerySet.get_or_create = _get_or_create
